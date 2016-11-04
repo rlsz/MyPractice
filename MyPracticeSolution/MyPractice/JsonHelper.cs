bd049de389b43c83e40b0d97493b093bafbff59f
rlsz <1264617582@qq.com>
2016-11-04 08:56:00 +0800

    jsonhelper
    code merger

--- MyPracticeSolution/MyPractice/JsonHelper.cs
+++ MyPracticeSolution/MyPractice/JsonHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,7 @@ namespace MyPractice
         /// </summary>
         public static string ToJsonString(this Object obj)
         {
-            return JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.None, TimeFormat);
+            return JsonConvert.SerializeObject(obj, Formatting.None, TimeFormat);
         }
         /// <summary>
         /// 反序列化Json字符串成为T的对象实例,如果成员类型为DateTime，则检测格式"yyyy-MM-dd HH:mm:ss"
@@ -31,7 +32,7 @@ namespace MyPractice
 
         public static string ToJsonString(this Object obj, params JsonConverter[] converters)
         {
-            return JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.None, converters);//请勿将Newtonsoft.Json.Formatting设置为缩进，否则将导致前台parseJSON函数失效，很多功能将会出现问题
+            return JsonConvert.SerializeObject(obj, Formatting.None, converters);//请勿将Newtonsoft.Json.Formatting设置为缩进，否则将导致前台parseJSON函数失效，很多功能将会出现问题
         }
         public static T DeserializeJsonString<T>(this String str, params JsonConverter[] converters)
         {
